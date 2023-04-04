9922321a8390251f4eed3f8410ea7983960fac2e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-04-03 18:59:31 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ BaseModel """
 import uuid
-import models
 from datetime import datetime
 
 
@@ -39,15 +38,3 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
-
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
