4eb762cdea89b2861d5e6b81beee2961fa5c55bb
oluwatobiloba <oluwateezzy03@gmail.com>
2023-04-07 06:53:06 -0700

    commit

--- console.py
+++ console.py
@@ -4,11 +4,14 @@ import cmd
 import sys
 import json
 import os
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ command """
     prompt = '(hbnb) '
+    classes =  {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
         """ quit program """
@@ -21,6 +24,72 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """ emptyline entered """
+        pass
+
+    def do_create(self, arg):
+        """ create instance of model """
+        if len(arg) == 0:
+            print('** class name missing **')
+            return
+        if arg:
+            lis_arg = arg.split()
+            if len(lis_arg) == 1:
+                if arg in self.classes.keys():
+                    new = self.classes[arg]()
+                    new.save()
+                    print(new.id)
+                else:
+                    print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """ show instance created """
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        if arg:
+            if arg.split()[0] not in self.classes:
+                print("** class doesn't exist **")
+                return
+        if len(arg.split()) > 1:
+            key = arg.split()[0] + '.' + arg.split()[1]
+            if key in storage.all():
+                val = storage.all()
+                print(val[key])
+            else:
+                 print('** no instance found **')
+        else:
+            print('** instance id missing **')
+
+    def do_destroy(self, arg):
+        """ delete an instance of a classe """
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        if arg:
+            if arg.split()[0] not in self.classes:
+                print("** class doesn't exist **")
+                return
+        if len(arg.split()) > 1:
+            key = arg.split()[0] + "." + arg.split()[1]
+            if key in storage.all():
+                value = storage.all()
+                del value[key]
+            else:
+                print("** no instance found **")
+        else:
+            print("** instance id missing **")
+
+    def do_all(self, arg):
+        """ all instances in storage """
+        if len(arg) == 0:
+            print([str(value) for value in storage.all().values()])
+        elif arg in self.classes:
+            print([str(value) for key, value in storage.all().items() if arg in key])
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """ update an instances """
         pass
 
 if __name__ == '__main__':
