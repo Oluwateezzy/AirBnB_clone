73e421b43b1cf4ecdac8f3313cc0b8a42db14228
oluwatobiloba <oluwateezzy03@gmail.com>
2023-04-10 05:25:59 -0700

    update

--- console.py
+++ console.py
@@ -7,12 +7,20 @@ import os
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.reviews import Reviews
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
     """ command """
     prompt = '(hbnb) '
-    classes =  {'BaseModel': BaseModel, 'User': User}
+    classes =  {'BaseModel': BaseModel, 'User': User,
+                'City': City, 'State': State,
+                'Place': Place, 'Amenity': Amenity,
+                'Reviews': Reviews}
 
     def do_quit(self, arg):
         """ quit program """
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" State """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ construct """
+    state_id = ""
+    name = ""
